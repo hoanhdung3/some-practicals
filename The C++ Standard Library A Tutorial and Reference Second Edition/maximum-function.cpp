39be378dbb732f2c2ec969e982beac88e94f26b1
Ho Anh Dung <84617443+hoanhdung3@users.noreply.github.com>
2022-01-26 16:39:57 +0700

    Create maximum-function.cpp

---
+++ The C++ Standard Library A Tutorial and Reference Second Edition/maximum-function.cpp
@@ -0,0 +1,21 @@
+// p.164
+#include<iostream>
+#include<algorithm>
+
+bool int_ptr_less(int *a, int *b){
+    return *a < *b;
+}
+
+int main(){
+    int x = 17;
+    int y = 42;
+    int z = 33;
+    int *px = &x;
+    int *py = &y;
+    int *pz = &z;
+    
+    int *max = std::max(px,py,int_ptr_less);
+    
+    std::pair<int*,int*> extremes = std::minmax({px,py,pz},int_ptr_less);
+    return 0;
+}
