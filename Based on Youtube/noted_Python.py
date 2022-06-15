627cfbdc461ac81763dc35b62b66aa0a5943c457
Ho Anh Dung <84617443+hoanhdung3@users.noreply.github.com>
2022-06-15 19:56:19 +0700

    Create noted_Python.py

---
+++ Based on Youtube/noted_Python.py
@@ -0,0 +1,30 @@
+# gg = [7,8,3]
+# for index, item in enumerate(gg):
+#     print(index, item)
+
+# gg = ('Kevin', 'Niklas', 'Jenny', 'Craig')
+# print(sorted(gg, key=lambda k: k[1])) # sorted by index 1 
+
+# gg = [i**2 for i in range(10) if i>4]
+# print(gg)
+# t = [1,2]
+# gg.extend(t)
+# print(gg)
+
+# gg = 1, # the comma tells Python it's tuple
+# print(gg, type(gg))
+# gg += (3,)
+# print(gg)
+
+# s1 = {1,2,3}
+# s2 = {3,4,5}
+# print(s1 & s2)
+# print(s1 | s2)
+# print(s1 ^ s2)
+# print(s1 - s2)
+# print(s1 <= s2) # check for subset
+# print(s1 >= s2) # check for subset
+
+# gg = {1 : 'Ho', 2 : "Anh", 3 : 'Dung'}
+# for k, v in gg.items():
+#     print(k , v)
